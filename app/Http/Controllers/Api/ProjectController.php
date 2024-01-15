96c26a34235b81aa7f73f79dd1bd80369ef0e269
edocico <138378767+edocico@users.noreply.github.com>
2024-01-15 18:06:23 +0100

    add type api

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        $projects = Project::with('type')->paginate(12);
         return response()->json([
             'success'=> true,
             'results'=> $projects,
